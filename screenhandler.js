a9f4411087cfdaa757578fdee3df3a7d976f0f29
zChar <105929248+zcharo99@users.noreply.github.com>
2024-07-13 19:20:27 +0200

    we are still not cooking yet

--- screenhandler.js
+++ screenhandler.js
@@ -1,2 +1,8 @@
 const levelSelector = document.getElementById('level-selector');
 
+if (window.screen.width < 800) {
+    levelSelector.style.display = 'none';
+    levelSelector.style.position = 'absolute';
+
+    const levelSelectorButton = document.createElement('a')
+}
